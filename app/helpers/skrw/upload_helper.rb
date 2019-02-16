84c128824307cc0add7af0dc553b347a5538bae4
R4ttlesnake <mail@pbernhard.com>
2019-02-16 16:27:11 +0100

    added file_srcset method to UploadHelper

--- app/helpers/skrw/upload_helper.rb
+++ app/helpers/skrw/upload_helper.rb
@@ -26,26 +26,46 @@ module Skrw
     # use:  file_srcset_image_tag(file, versions: [:small, :medium, :large]
     #                             default: :large, options)
 
-    def file_srcset_image_tag(file, versions: [], default: nil, **options)
-      
-      # with srcset / src if versions else only src
+    def file_srcset(file, versions: [])
       if file.is_a?(Hash) && versions.any?
-
-        # set src to default or first of versions
-        src = default.nil? ? file.values.first.url : file[default].url
-        
-        # build srcset and merge with image options
-        srcset = versions.map { |v| "#{file[v].url} #{file[v].metadata['width']}w" }.join(', ')
-        options.merge!({ srcset: srcset })
-
-        # output image_tag with srcset (merged into options)
-        image_tag(src, options)
+        return versions.map { |v| "#{file[v].url} #{file[v].metadata['width']}w" }.join(', ')
       else
-
-        # output image_tag with src only
-        src = file.is_a?(Hash) ? file.values.first.url : file.url
-        image_tag(src, options)
+        return nil
       end
+    end
+
+    def file_srcset_image_tag(file, versions: [], default: nil, **options)
+      src = nil
+
+      if file.is_a?(Hash)
+        src = default.nil? ? file.values.first.url : file[default].url
+      else
+        src = file.url
+      end
+      
+      srcset = file_srcset(file, versions: versions)
+      options.merge!({ srcset: srcset })
+
+      image_tag(src, options)
+
+      # # with srcset / src if versions else only src
+      # if file.is_a?(Hash) && versions.any?
+
+      #   # set src to default or first of versions
+      #   src = default.nil? ? file.values.first.url : file[default].url
+        
+      #   # build srcset and merge with image options
+      #   srcset = versions.map { |v| "#{file[v].url} #{file[v].metadata['width']}w" }.join(', ')
+      #   options.merge!({ srcset: srcset })
+
+      #   # output image_tag with srcset (merged into options)
+      #   image_tag(src, options)
+      # else
+
+      #   # output image_tag with src only
+      #   src = file.is_a?(Hash) ? file.values.first.url : file.url
+      #   image_tag(src, options)
+      # end
     end
 
     # uploads panel for uploads within scope of parent
