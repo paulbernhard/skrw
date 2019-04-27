f37e4eb67c5ea53c423eed9e084552f30780d747
R4ttlesnake <mail@pbernhard.com>
2019-04-27 10:16:16 +0200

    fixed dependency issues

--- app/controllers/skrw/passwords_controller.rb
+++
@@ -1,8 +0,0 @@
-# TODO implement recovery functionality for user.rb --> setup and test passwords controller and views
-
-# require_dependency "skrw/application_controller"
-
-# module Skrw
-#   class PasswordsController < Devise::PasswordsController
-#   end
-# end
--- app/mailers/skrw/user_mailer.rb
+++
@@ -1,9 +0,0 @@
-# TODO implement recovery functionality for user.rb --> setup and test mailer
-
-# module Skrw
-#   class UserMailer < Devise::Mailer
-#     helper :application
-#     include Devise::Controllers::UrlHelpers
-#     default template_path: 'devise/mailer'
-#   end
-# end
