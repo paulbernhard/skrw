f4a3210a6375550d8e7e8fd99abb8ddad7a72d9d
R4ttlesnake <mail@pbernhard.com>
2019-08-17 19:00:11 +0200

    moved styles to skrw-js package / started to remove simple form dependency

--- app/helpers/skrw/application_helper.rb
+++ app/helpers/skrw/application_helper.rb
@@ -1,16 +1,16 @@
 module Skrw
   module ApplicationHelper
 
-    def date(date, time: false)
+    def skrw_date(date, time: false)
       date_format = "%d.%m.%Y"
       time_format = "%H:%M:%S"
       format = time ? time_format : date_format
       date.strftime format
     end
 
-    def timestamps(record, show_created: false)
-      dates = ["updated at: #{date(record.updated_at, time: true)}"]
-      dates = ["created at: #{date(record.created_at, time: true)}"] + dates if show_created
+    def skrw_timestamps(record, show_created: false)
+      dates = ["updated at: #{skrw_date(record.updated_at, time: true)}"]
+      dates = ["created at: #{skrw_date(record.created_at, time: true)}"] + dates if show_created
       dates.join(", ")
     end
   end
--- app/helpers/skrw/form_helper.rb
+++ app/helpers/skrw/form_helper.rb
@@ -7,5 +7,11 @@ module Skrw
         data: { controller: "skrw--form" } })
       simple_form_for(object, *(args << options), &block)
     end
+
+    def skrw_form_errors(form)
+      if form.object.errors.any?
+        render "skrw/shared/errors", object: form.object
+      end
+    end
   end
 end
