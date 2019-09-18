6fe4944dc7195e4f069b425d71d8ba22be8a036d
R4ttlesnake <mail@pbernhard.com>
2019-09-18 21:00:07 +0200

    reworked form builder

--- app/forms/skrw/form_builder.rb
+++ app/forms/skrw/form_builder.rb
@@ -1,19 +1,42 @@
 class Skrw::FormBuilder < ActionView::Helpers::FormBuilder
-  include ActionView::Helpers::TagHelper
 
-  def label(attr, text = nil, **options, &block)
-    if @object.errors[attr].any?
-      options[:class] = (options[:class].to_s + " skrw-label--erroneous").strip
+  # field wrapper with label and error messages
+  def field(attr, desc = nil, **options, &block)
+    options[:class] = "#{options[:class]} skrw-field".strip
+
+    # add erroneous class
+    object_errors = @object.errors[attr]
+    options[:class] += " skrw-field--erroneous" if object_errors.any?
+
+    # build output
+    output = "".html_safe
+    output += self.label(attr, desc) unless desc.nil?
+    output += @template.capture(&block)
+    output += self.errors(object_errors) if object_errors.any?
+
+
+    @template.content_tag(:div, options) do
+      output.html_safe
     end
-    super(attr, text = nil, **options, &block)
   end
 
-  def text_field(attr, **options)
-    render_with_errors(attr) do
-      super(attr, options)
+  def label(attr, text = nil, options = {}, &block)
+    if text
+      presence_validators = @object.class.validators.select { |v| v.is_a?(ActiveRecord::Validations::PresenceValidator) && v.attributes.include?(attr) }
+      text += " *" if presence_validators.any?
+    end
+
+    super(attr, text, options, &block)
+  end
+
+  # field errors
+  def errors(errors = [])
+    @template.content_tag(:div, class: "skrw-errors") do
+      errors.map { |e| @template.content_tag(:div, e) }.join("").html_safe
     end
   end
 
+  # base errors
   def base_errors
     errors = @object.errors[:base]
     if errors.any?
@@ -23,16 +46,17 @@ class Skrw::FormBuilder < ActionView::Helpers::FormBuilder
     end
   end
 
-  def render_with_errors(attr, &block)
-    errors = @object.errors[attr]
-    if errors.any?
-      @template.content_tag(:div, class: "skrw-field--erroneous") do
-        output = yield
-        output += errors.map { |e| @template.content_tag(:div, e) }.join("").html_safe
-        output
-      end
-    else
-      yield
-    end
-  end
+  # render method with optional error messages
+  # def render_with_errors(attr, &block)
+  #   errors = @object.errors[attr]
+  #   if errors.any?
+  #     @template.content_tag(:div, class: "skrw-field--erroneous") do
+  #       output = yield
+  #       output += errors.map { |e| @template.content_tag(:div, e) }.join("").html_safe
+  #       output
+  #     end
+  #   else
+  #     yield
+  #   end
+  # end
 end
